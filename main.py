a3dc6e17d726ebe9803d9fb51b632a94621bef17
RaimeiRaikiri <mikescutts10@gmail.com>
2024-09-01 16:00:27 +0100

    moved the boards surface to center of window and added a board border

--- main.py
+++ main.py
@@ -8,13 +8,18 @@ screen = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption('2-Player Chess')
 boards = Board()
 
+def DrawBoardBorder():
+    # Draws a rect border around the board to prevent the pieces from leaving the board
+    pygame.draw.rect(screen, (0,0,0), boards.surface.get_rect(topleft=(200,100)), width = 2)
+    
 while True:
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-    screen.fill('black')
-    screen.blit(boards.surface, (0,0))
+    screen.fill('white')
+    screen.blit(boards.surface, (200,100))
+    DrawBoardBorder()
                 
     pygame.display.flip()
     clock.tick(60)
