7d7530ea781ab498b03d54daba5f2deb33a54ae0
RaimeiRaikiri <mikescutts10@gmail.com>
2024-09-01 18:56:50 +0100

    moved order of variable declaration so a method could be called at the right time, added method that get starting position of rect based on index

--- PieceClasses/piece.py
+++ PieceClasses/piece.py
@@ -2,19 +2,21 @@ import pygame
 
 class piece:
     
-    def __init__(self, color, indexIOnArray: int, indexJOnArray: int, boardArray = list[list]):
-        # Visual variables
-        self.size = (50,50)
-        self.color = color
-        self.surface = self.reduce_image_size()
-        self.rect =  self.surface.get_rect(center=(300,100))
-        
+    def __init__(self, color, indexIOnArray: int, indexJOnArray: int, boardArray: list[list]):
         # Positional variables
         self.indexI = indexIOnArray
         self.indexJ = indexJOnArray
         self.moves = None
         self.board = boardArray
         self.onBoard = True
+        self.x, self.y = self.get_position()
+        
+        # Visual variables
+        self.size = (50,50)
+        self.color = color
+        self.surface = self.reduce_image_size()
+        self.rect =  self.surface.get_rect(center=(self.x, self.y))
+        
     
     
     # Get image and fit it to size of piece
@@ -24,3 +26,6 @@ class piece:
     def reduce_image_size(self):
         return pygame.transform.scale(self.get_image(), self.size)
         
+    # Initial position set by index position
+    def get_position(self):
+        return ((self.indexJ+1)*100, (self.indexI+1)*100)
