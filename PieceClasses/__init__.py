8ffd1f29d2957017798fb23f9aa17474d5da9388
RaimeiRaikiri <mikescutts10@gmail.com>
2024-09-01 16:51:59 +0100

    added init file for the folder to make accessing the classes easier

---
+++ PieceClasses/__init__.py
@@ -0,0 +1,7 @@
+from PieceClasses.piece import piece
+from PieceClasses.castle import Castle
+from PieceClasses.bishop import Bishop
+from PieceClasses.king import King
+from PieceClasses.knight import Knight
+from PieceClasses.queen import Queen
+from PieceClasses.pawn import Pawn
