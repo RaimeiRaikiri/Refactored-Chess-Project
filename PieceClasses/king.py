5da4e29f346291783417a64cddab04b24b90bce9
RaimeiRaikiri <mikescutts10@gmail.com>
2024-10-01 13:59:00 +0100

    added moved variable and castling to the get moves method

--- PieceClasses/king.py
+++ PieceClasses/king.py
@@ -4,11 +4,12 @@ from PieceClasses.piece import piece
 class King(piece):
     def __init__(self, color, indexIOnArray: int, indexJOnArray: int, boardArray: list[list]):
         super().__init__(color, indexIOnArray, indexJOnArray, boardArray)
+        self.moved = False
     
     def get_image(self):
         return pygame.image.load(f'./PieceClasses/images/{self.color}pieces/{self.color}king.png')
 
-    def get_moves(self):
+    def get_moves(self, castle1, castle2, opposition_moves):
             moves = []
             if self.color == 'white':
                 # top
@@ -43,6 +44,31 @@ class King(piece):
                 if self.indexI - 1 < 8 and self.indexJ-1 >=0:
                     if self.board[self.indexI-1][self.indexJ-1] < 1:
                         moves.append((self.indexI-1,self.indexJ-1))
+                
+                # Castling
+                if self.moved == False and castle1.moved == False:
+                    # Long side
+                    
+                    # Spaces between king and castle empty
+                    if self.board[self.indexI][self.indexJ - 1] == 0 and self.board[self.indexI][self.indexJ - 2] == 0 and self.board[self.indexI][self.indexJ - 3] == 0:
+                        moves.append((self.indexI, self.indexJ-2))
+                        spaces = [(self.indexI, self.indexJ),(self.indexI, self.indexJ -1), (self.indexI,self.indexJ-2), (self.indexI, self.indexJ-3)]
+                        for space in spaces:
+                            if space in opposition_moves:
+                                moves.pop()
+                                break
+                    
+                if self.moved == False and castle2.moved == False:
+                    # Short side
+                    
+                    # Spaces between king and castle empty
+                    if self.board[self.indexI][self.indexJ + 1] == 0 and self.board[self.indexI][self.indexJ + 2] == 0:
+                        moves.append((self.indexI, self.indexJ+2))
+                        spaces = [(self.indexI, self.indexJ),(self.indexI, self.indexJ +1), (self.indexI,self.indexJ+2)]
+                        for space in spaces:
+                            if space in opposition_moves:
+                                moves.pop()
+                                break
                 
                 return moves
             
@@ -79,5 +105,30 @@ class King(piece):
                 if self.indexI - 1 >= 0 and self.indexJ-1 >= 0:
                     if self.board[self.indexI-1][self.indexJ-1] >= 0:
                         moves.append((self.indexI-1,self.indexJ-1))
+                
+                # Castling
+                if self.moved == False and castle1.moved == False:
+                    # Long side
+                    
+                    # Spaces between king and castle empty
+                    if self.board[self.indexI][self.indexJ - 1] == 0 and self.board[self.indexI][self.indexJ - 2] == 0 and self.board[self.indexI][self.indexJ - 3] == 0:
+                        moves.append((self.indexI, self.indexJ-2))
+                        spaces = [(self.indexI, self.indexJ),(self.indexI, self.indexJ -1), (self.indexI,self.indexJ-2), (self.indexI, self.indexJ-3)]
+                        for space in spaces:
+                            if space in opposition_moves:
+                                moves.pop()
+                                break
+                    
+                if self.moved == False and castle2.moved == False:
+                    # Short side
+                    
+                    # Spaces between king and castle empty
+                    if self.board[self.indexI][self.indexJ + 1] == 0 and self.board[self.indexI][self.indexJ + 2] == 0:
+                        moves.append((self.indexI, self.indexJ+2))
+                        spaces = [(self.indexI, self.indexJ),(self.indexI, self.indexJ +1), (self.indexI,self.indexJ+2)]
+                        for space in spaces:
+                            if space in opposition_moves:
+                                moves.pop()
+                                break
                 
                 return moves
